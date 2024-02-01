f19360c316be425f50557d6d69c0ee85784d0473
Mateusz <mkaminski.contact@gmail.com>
2024-02-01 11:11:45 +0100

    added dummy header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import Navigation from "./components/Header"
 function App() {
+
 
 
   return (
     <>
+    <Navigation/>
 
 Collections
   Men
---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+import logo from '../assets/logo.svg'
+
+export default function Logo() {
+    return <a className='p-2' href="#"><img src={logo} alt="Sneakers Logo" /></a>
+};
