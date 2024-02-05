b3ae3e19fe8c09dd6903f8598ee7d8308cc076e1
Mateusz <mkaminski.contact@gmail.com>
2024-02-05 18:03:56 +0100

    fixed nav ul translate-x

--- src/components/Header/Navigation/NavList.tsx
+++ src/components/Header/Navigation/NavList.tsx
@@ -21,10 +21,10 @@ export default function NavList({ isActive }: NavListProps) {
 	const anchorClasses =
 		"lg:pb-[39px] lg:px-2 lg:py-2 lg:border-b-4  lg:relative  lg:hover:border-orange-400 lg:hover:text-black lg:text-slate-700 transition lg:inline z-100";
 	const ulClasses = isMobile
-		? "flex absolute mt-14 flex-col gap-4 duration-300 translate-x-[-200%]"
+		? "flex absolute mt-14 flex-col gap-4 duration-300 "
 		: "lg:flex lg:flex-row lg:relative gap-4";
 
-	const navIsOpenClasses = isActive ? "translate-x-0" : "";
+	const navIsOpenClasses = isActive ? "translate-x-0" : "translate-x-[-200%]";
 	return (
 		<ul className={`${ulClasses} ${navIsOpenClasses}`}>
 			{navNames.map((name, index) => (
