df44963e798d8d63dfb25c6a35edd3e7aa62bc58
Mateusz <mkaminski.contact@gmail.com>
2024-02-14 17:10:32 +0100

    added item counting

--- src/components/product/product-detail-section/ProductDetailsSection.tsx
+++ src/components/product/product-detail-section/ProductDetailsSection.tsx
@@ -1,19 +1,20 @@
 import ProductDetails from "./ProductDetails";
 import ProductPricing from "./ProductPricing";
 import ProductQuantityControl from "./ProductQuantityControl";
-import cartIcon from '../../../assets/icon-cart-white.svg'
+import cartIcon from "../../../assets/icon-cart-white.svg";
 
 export default function ProductDetailSection() {
-	return <section className='p-6 lg:p-10 flex flex-col lg:justify-center lg:mb-40'>
-        <ProductDetails/>
-        <ProductPricing/>
-        <div className="lg:flex lg:gap-4">
-
-        <ProductQuantityControl/>
-        <button className='bg-orange-500 flex w-full px-4 py-3 items-center justify-center rounded-md my-2 gap-3 hover:bg-orange-400 focus:bg-orange-400 duration-200 lg:w-3/4'>
+	return (
+		<section className='p-6 lg:p-10 flex flex-col lg:justify-center lg:mb-40'>
+			<ProductDetails />
+			<ProductPricing />
+			<div className='lg:flex lg:gap-4'>
+				<ProductQuantityControl />
+				<button className='bg-orange-500 flex w-full px-4 py-3 items-center justify-center rounded-md my-2 gap-3 hover:bg-orange-400 focus:bg-orange-400 duration-200 lg:w-3/4'>
 					<img src={cartIcon} style={{ color: "white" }} alt='' />
-					<span className="font-bold text-white">Add to cart</span>
+					<span className='font-bold text-white'>Add to cart</span>
 				</button>
-        </div>
-    </section>;
+			</div>
+		</section>
+	);
 }
--- src/components/product/product-detail-section/ProductQuantityControl.tsx
+++ src/components/product/product-detail-section/ProductQuantityControl.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import minus from "../../../assets/icon-minus.svg";
 import plus from "../../../assets/icon-plus.svg";
 
 export default function ProductQuantityControl() {
+	const [itemQuantity, setItemQuantity] = useState(0);
+
+	const incrementQuantity = () => setItemQuantity(prevQuantity => prevQuantity + 1);
+	const decrementQuantity = () =>
+		setItemQuantity(prevQuantity => (prevQuantity > 0 ? prevQuantity - 1 : 0));
 	return (
 		<div className='flex justify-between bg-slate-200 items-center px-4 py-3 my-2 rounded-md lg:w-1/3'>
-			<button className='p-2 hover:scale-125 focus:scale-125 duration-150'>
+			<button
+				onClick={decrementQuantity}
+				className='p-2 hover:scale-125 focus:scale-125 duration-150'>
 				<img src={minus} alt='minus button' />
 			</button>
-			<span>0</span>
-			<button className='p-2 hover:scale-125 focus:scale-125 duration-150'>
+			<span>{itemQuantity}</span>
+			<button
+				onClick={incrementQuantity}
+				className='p-2 hover:scale-125 focus:scale-125 duration-150'>
 				<img src={plus} alt='plus button' />
 			</button>
 		</div>
