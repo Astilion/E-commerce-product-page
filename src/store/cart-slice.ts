64697c9ebaff4d2a01f270a71daeaa90c01e5d7f
Mateusz <mkaminski.contact@gmail.com>
2024-02-14 16:31:13 +0100

    updated cart slice

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -86,4 +86,5 @@ const cartSlice = createSlice({
 	},
 });
 
+export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import deviceTypeReducer from "./deviceType-slice";
+import cartReducer from "./cart-slice";
 export const store = configureStore({
 	reducer: {
 		deviceType: deviceTypeReducer,
+		cart: cartReducer,
 	},
 });
-
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
