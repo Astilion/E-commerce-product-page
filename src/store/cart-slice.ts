de1282b2f3edc0d2e0236b46cea9d04f1fa5088e
Mateusz <mkaminski.contact@gmail.com>
2024-02-14 16:26:00 +0100

    created cart-slice

---
+++ src/store/cart-slice.ts
@@ -0,0 +1,89 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Product {
+	id: string;
+	quantity: number;
+}
+interface CartState {
+	totalQuantity: number;
+	products: Product[];
+	totalPrice: number | null;
+}
+const initialState: CartState = {
+	totalQuantity: 0,
+	products: [],
+	totalPrice: null,
+};
+
+const cartSlice = createSlice({
+	name: "cart",
+	initialState,
+	reducers: {
+		addProductToCart(state, action: PayloadAction<Product>) {
+			const productIndex = state.products.findIndex(
+				product => product.id === action.payload.id
+			);
+
+			state.totalQuantity += action.payload.quantity;
+
+			if (productIndex !== -1) {
+				// If product already exists, update the quantity
+				state.products[productIndex].quantity += action.payload.quantity;
+			} else {
+				// If product doesn't exist, add it to the cart
+				state.products.push(action.payload);
+			}
+		},
+		replaceCart(state, action) {
+			state.products = action.payload.products;
+			state.totalQuantity = action.payload.totalQuantity;
+		},
+		removeItemsFromCart(state, action) {
+			const productIdToRemove = action.payload.id;
+			const removeQuantity = action.payload.quantity;
+
+			const productIndex = state.products.findIndex(
+				product => product.id === productIdToRemove
+			);
+
+			if (productIndex !== -1) {
+				const existingProduct = state.products[productIndex];
+
+				if (
+					existingProduct.quantity === 1 ||
+					existingProduct.quantity <= removeQuantity
+				) {
+					state.totalQuantity -= existingProduct.quantity;
+					state.products.splice(productIndex, 1);
+				} else {
+					existingProduct.quantity -= removeQuantity;
+					state.totalQuantity -= removeQuantity;
+				}
+			}
+		},
+		handleProductQuantity(state, action) {
+			const productIdToHandle = action.payload.id;
+			const newQuantity = Number(action.payload.quantity);
+
+			const productIndex = state.products.findIndex(
+				product => product.id === productIdToHandle
+			);
+
+			if (productIndex !== -1) {
+				const existingProduct = state.products[productIndex];
+
+				if (newQuantity === 0) {
+					// Remove the entire product if newQuantity is 0
+					state.totalQuantity -= existingProduct.quantity;
+					state.products.splice(productIndex, 1);
+				} else {
+					// Update the quantity of the existing product
+					state.totalQuantity += newQuantity - existingProduct.quantity;
+					existingProduct.quantity = newQuantity;
+				}
+			}
+		},
+	},
+});
+
+export default cartSlice.reducer;
