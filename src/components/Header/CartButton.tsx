9c49e96ff61a26d82a5026ec306765298a3b3215
Mateusz <mkaminski.contact@gmail.com>
2024-02-14 17:34:23 +0100

    added cart quantity icon

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Cart from "./cart/Cart";
 import cartIcon from "../../assets/icon-cart.svg";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 export default function CartButton() {
 	const [cartIsOpen, setCartIsOpen] = useState(false);
@@ -8,12 +10,19 @@ export default function CartButton() {
 		setCartIsOpen(prev => !prev);
 	};
 
+	const cartItemsQuantity = useSelector(
+		(state: RootState) => state.cart.totalQuantity
+	);
+
 	return (
 		<div>
-			<button
-				className='p-2 '
-				onClick={toggleCart}>
+			<button className='p-2 relative ' onClick={toggleCart}>
 				<img src={cartIcon} alt='Cart' />
+				{cartItemsQuantity > 0 && (
+					<span className='bg-orange-500 rounded-[100%] px-[8px] absolute top-0 right-0 text-[10px] text-white font-bold'>
+						{cartItemsQuantity}
+					</span>
+				)}
 			</button>
 			{cartIsOpen && <Cart />}
 		</div>
