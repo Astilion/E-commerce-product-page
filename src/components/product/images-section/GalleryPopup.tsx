0a7d585598a576e86b63a072a3ab7b6ed7514903
Mateusz <mkaminski.contact@gmail.com>
2024-02-21 12:25:47 +0100

    fixed popup button

--- src/components/product/images-section/GalleryPopup.tsx
+++ src/components/product/images-section/GalleryPopup.tsx
@@ -21,9 +21,12 @@ export default function GalleryPopup({
 	return (
 		<Modal onClose={onClose}>
 			<div className='flex w-full justify-center items-center overflow-hidden lg:rounded-2xl  z-[90]'>
-				<div className='flex w-1/2 overflow-hidden rounded-2xl'>
+				<div className=" absolute flex w-[40%]">
+
 				<PrevButton onClick={prevImageHandler} />
 				<NextButton onClick={nextImageHandler} />
+				</div>
+				<div className='relative flex w-[40%] overflow-hidden rounded-2xl'>
 					<ImageSlider images={images} currentIndex={currentIndex} />
 				</div>
 			</div>
--- src/components/product/images-section/buttons/PrevButton.tsx
+++ src/components/product/images-section/buttons/PrevButton.tsx
@@ -7,7 +7,7 @@ export default function PrevButton({ onClick }: PrevButtonProps) {
 	return (
 		<button
 			onClick={onClick}
-			className='bg-white rounded-[100%] absolute top-1/2 transform -translate-y-1/2 p-4 left-4 hover:bg-orange-500 focus:bg-orange-500 duration-150 z-20'
+			className='bg-white rounded-[100%] absolute top-1/2 transform -translate-y-1/2 p-4 left-4 lg:-left-6  hover:bg-orange-500 focus:bg-orange-500 duration-150 z-20'
 			aria-label='Show Previous Image'>
 			<img className='h-4 w-4' src={previousIcon} alt='Previous Button' />
 		</button>
